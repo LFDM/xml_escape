3b0c8b6c2349905297f2aa6bfc948901cba20aba
LFDM <1986gh@gmail.com>
2014-01-20 20:51:58 +0100

    Refactors specs

--- spec/lib/xml_escape_spec.rb
+++ spec/lib/xml_escape_spec.rb
@@ -5,17 +5,18 @@ describe XmlEscape do
     Class.new { include XmlEscape }.new
   end
 
+  let(:txt) { '& text, - " \' < >' }
+  let(:encoded_txt) { 'arma &amp; &quot;virum&quot;' }
+
   describe "#xml_encode" do
     it "encodes xml characters" do
-      txt = '& text, - " \' < >'
       dummy.xml_encode(txt).should == '&amp; text, - &quot; &apos; &lt; &gt;'
     end
   end
 
   describe "#xml_decode" do
     it "decodes xml characters" do
-      txt = 'arma &amp; &quot;virum&quot;'
-      dummy.xml_decode(txt).should == 'arma & "virum"'
+      dummy.xml_decode(encoded_txt).should == 'arma & "virum"'
     end
   end
 end
